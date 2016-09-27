a24414cd0e96599b38c7a0a5536449f147d5a2b5
Jiří Fiala <jfiala@redhat.com>
2016-09-27 13:45:47 +0200

    initial commit

---
+++ grunt-settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    sassDir:          'scss/',
+    sassMainFileName: 'main',
+    cssDir:           'css/',
+    cssMainFileDir:   'css/',
+    cssMainFileName:  'main',
+    jsDir:            'js/',
+    imgDir:           'images/',
+    imgSourceDir:     'sourceimages/',
+    //hasBower: {includePaths: ['bower_components/foundation/scss']},
+    distDir:          'dist/'
+  };
